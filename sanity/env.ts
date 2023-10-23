89e5af3284e2530eaeda54f166efd2a1c7f6ac54
joel <joelhooks@gmail.com>
2023-10-23 12:54:39 -0700

    add turbo and fix build

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,14 +1,14 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-10-20'
+  process.env.SANITY_STUDIO_API_VERSION || '2023-10-20'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+  process.env.SANITY_STUDIO_DATASET,
+  'Missing environment variable: SANITY_STUDIO_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+  process.env.SANITY_STUDIO_PROJECT_ID,
+  'Missing environment variable: SANITY_STUDIO_PROJECT_ID'
 )
 
 export const useCdn = false
